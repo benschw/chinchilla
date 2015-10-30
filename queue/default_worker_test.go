29c1d71c889df472fc81a29ea15611bca8e61313
Ben Schwartz <benschw@gmail.com>
2015-10-30 09:45:19 -0500

    updating test

--- queue/default_worker_test.go
+++ queue/default_worker_test.go
@@ -3,23 +3,11 @@ package queue
 import (
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/benschw/chinchilla/config"
 	"github.com/benschw/chinchilla/example/ex"
-	"github.com/streadway/amqp"
 	"github.com/stretchr/testify/assert"
 )
-
-var conn *amqp.Connection
-
-func init() {
-	c, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	if err != nil {
-		panic(err)
-	}
-	conn = c
-}
 
 func TestDefaultWorkerConsume(t *testing.T) {
 	// given
@@ -51,22 +39,4 @@ func TestDefaultWorkerConsume(t *testing.T) {
 	cnt := countMessages(msgs)
 
 	assert.Equal(t, 10, cnt, "wrong number of msgs")
-}
-
-func countMessages(msgs <-chan amqp.Delivery) int {
-
-	var cnt = 0
-	for {
-		select {
-		case d, _ := <-msgs:
-			d.Ack(false)
-			if d.Body == nil {
-				return cnt
-			}
-			cnt++
-		case <-time.After(5 * time.Millisecond):
-			return cnt
-		}
-	}
-	return 0
 }
