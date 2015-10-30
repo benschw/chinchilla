119fbc49bc26ba694f08fe552a81002df4f8eee4
Philip W <philip.whitt@sbcglobal.net>
2015-10-30 13:35:31 -0500

    Adding more topic tests

--- queue/topic_test.go
+++ queue/topic_test.go
@@ -42,3 +42,44 @@ func TestTopicConsume(t *testing.T) {
 
 	assert.Equal(t, 10, cnt, "wrong number of msgs")
 }
+
+func TestTopicConsumeGlob(t *testing.T) {
+	// given
+	epCfg := config.EndpointConfig{
+		QueueConfig: map[interface{}]interface{}{
+			"queuename":    "foos",
+			"topicname":    "foo.update",
+			"exchangename": "demo",
+		},
+	}
+
+	publisher := &ex.Publisher{
+		Conn:   conn,
+		Config: &epCfg,
+	}
+
+	topic := &Topic{}
+
+	ch, _ := conn.Channel()
+	defer ch.Close()
+
+	// when
+	epCfg = config.EndpointConfig{
+		QueueConfig: map[interface{}]interface{}{
+			"queuename":    "foos",
+			"topicname":    "foo.*",
+			"exchangename": "demo",
+		},
+	}
+	msgs, err := topic.Consume(ch, epCfg)
+
+	for i := 0; i < 10; i++ {
+		publisher.PublishTopic(fmt.Sprintf("test topic: #%d", i), "text/plain")
+	}
+
+	// then
+	assert.Nil(t, err)
+	cnt := countMessages(msgs)
+
+	assert.Equal(t, 10, cnt, "wrong number of msgs")
+}
